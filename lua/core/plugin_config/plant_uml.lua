8a8af17a523216b9ae652c1de4e1072c2bbc8a63
geokoko <geokoko3@outlook.com.gr>
2025-04-09 00:00:01 +0300

    Minor fix in puml setup

--- lua/core/plugin_config/plant_uml.lua
+++ lua/core/plugin_config/plant_uml.lua
@@ -1,5 +1,5 @@
 require('soil').setup({
-	plantuml_jar_path = "~/plantuml/plantuml.jar", -- where your plantuml.jar is
+	puml_jar = "~/plantuml/plantuml.jar", -- where your plantuml.jar is
   	render_on_write = true, -- Automatically render on save
 })
 
