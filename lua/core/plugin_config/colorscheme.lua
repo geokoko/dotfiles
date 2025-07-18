f7392bfe022bac42720d5f07db5f78aed55d8172
geokoko <geokoko3@outlook.com.gr>
2025-07-18 15:20:29 +0300

    Set a default colorscheme (sonokai)

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,6 +1,6 @@
--- Colors and Appearance
-vim.opt.background = 'dark'             -- Dark background
-vim.opt.termguicolors = true           	-- True color support
-vim.cmd('colorscheme tokyodark')			-- Use colorscheme
-vim.cmd('syntax on')                    -- Enable syntax highlighting
+-- Colors and Appearance (choose your settings here)
+vim.opt.background = 'dark'             	-- Dark background
+vim.opt.termguicolors = true           		-- True color support
+vim.cmd('colorscheme sonokai')				-- Use colorscheme 
+vim.cmd('syntax on')                   		-- Enable syntax highlighting
 -- vim.cmd('set t_Co=256')					-- Force 256 color support
