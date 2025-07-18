2745df3bf0bf25f3c1dcee829efb7115e36afd79
geokoko <geokoko3@outlook.com.gr>
2025-07-18 14:37:18 +0300

    Fixed mason configuration

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,14 +1,40 @@
-require('mason-lspconfig').setup({
-    ensure_installed = { "lua_ls", "clangd" },  
-    automatic_installation = true,
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
+
+-- Set up Mason
+mason.setup()
+
+-- Ensure these LSPs are installed
+mason_lspconfig.setup({
+  ensure_installed = {
+    "lua_ls",
+	-- Lua server is recommended for easier setup of the configuration
+    -- Add the rest of your preferred servers here
+  },
 })
 
+-- DEPRECATED AFTER MASON 2.0.0 release. See https://github.com/mason-org/mason-lspconfig.nvim/releases/tag/v2.0.0
+-- Setup each installed LSP
+--mason_lspconfig.setup_handlers {
+--  function(server_name)
+--    lspconfig[server_name].setup({})
+--  end,
+--}
 
-local lspconfig = require('lspconfig')
-local lsp_defaults = lspconfig.util.default_config
-
-require('mason-lspconfig').setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({})
-    end
+-- Replaced by the vim.lsp.config() API
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {
+          'vim',
+          'require',
+        },
+      },
+    },
+  },
 })
