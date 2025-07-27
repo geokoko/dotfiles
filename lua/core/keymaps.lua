4adcc0d1734ae79732266d7f994a43f42b9af2f6
geokoko <geokoko3@outlook.com.gr>
2025-07-27 22:41:00 +0300

    Add LSP diagnostic keybindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<F8>', ':TagbarToggle<CR>', opts)
 -- Tab Navigation
 vim.keymap.set('n', '<A-Right>', ':tabnext<CR>', opts)
 vim.keymap.set('n', '<A-Left>', ':tabprevious<CR>', opts)
+
+-- LSP Diagnostics
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
